3c0c1522212d761ff284dd57c6db98e85655b6e4
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2023-01-30 22:22:59 +0530

    Add files via upload

---
+++ myfirstpro.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int a,b;
+    cout<<"hello world!"<<endl;
+    cout<<"enter the the two no"<<endl;
+    cin>>a>>b;
+    int s=a+b;
+    cout<<"sum is:"<<s;
+return 0;
+}
